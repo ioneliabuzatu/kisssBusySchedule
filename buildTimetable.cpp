84031d7d12b8c2e0509762768d81a81d9e303baf
Ionelia Buzatu <buzatuionelia@gmail.com>
2020-08-10 21:12:57 +0200

    new screeshot

--- buildTimetable.cpp
+++ buildTimetable.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
-    MainWindow window;
-    window.resize(640, 700);
+    Gui window;
+    window.resize(1000, 700);
     window.show();
     return app.exec();
 }
